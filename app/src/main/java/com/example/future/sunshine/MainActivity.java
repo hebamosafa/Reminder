9322f839cb33624486ce80e89eb248fb282e7f11
hebamosafa <36491097+hebamosafa@users.noreply.github.com>
2019-07-05 17:56:34 +0200

    some changes in main

--- app/src/main/java/com/example/future/sunshine/MainActivity.java
+++ app/src/main/java/com/example/future/sunshine/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.future.sunshine;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.LoaderManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
@@ -15,6 +18,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.SystemClock;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
@@ -24,12 +28,15 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -65,12 +72,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     String[] listItem;
     Mycorsuradaptor mCursorAdapter;
     private static final int ALARM_LOADER = 1;
-
+    Uri current_data_alarm;
+    Uri current_delete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView studentListView = findViewById(R.id.listView);
+        final ListView studentListView = findViewById(R.id.listView);
         Button fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,7 +121,64 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
-        getLoaderManager().initLoader(ALARM_LOADER, null, this);
+        studentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent=new Intent(MainActivity.this,add.class);
+        current_data_alarm= ContentUris.withAppendedId(AlarmEntry.CONTENT_URI,id);
+        intent.setData(current_data_alarm);
+        intent.putExtra("ID",id);
+        startActivity(intent);
+    }
+});
+  studentListView.setChoiceMode(studentListView.CHOICE_MODE_MULTIPLE_MODAL);
+  studentListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+      @Override
+      public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+          final  int checked_count=studentListView.getCheckedItemCount();
+          mode.setTitle(checked_count+"selected");
+      }
+
+      @Override
+      public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+         mode.getMenuInflater().inflate(R.menu.menu_add,menu);
+          return true;
+      }
+
+      @Override
+      public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+          return false;
+      }
+
+      @Override
+      public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+          long[] checkedItemPositions =studentListView.getCheckedItemIds();
+          int itemCount = studentListView.getCheckedItemCount();
+
+          for(int i=itemCount-1; i >= 0; i--){
+
+
+                  current_delete= ContentUris.withAppendedId(AlarmEntry.CONTENT_URI,checkedItemPositions[i]);
+
+                  int delete_only=  getContentResolver().delete(current_delete,null,null);
+                 // Toast.makeText(MainActivity.this, "Checked"+itemCount+"deleted"+delete_only, Toast.LENGTH_SHORT).show();
+
+          }
+
+
+
+
+
+       return true;
+      }
+
+      @Override
+      public void onDestroyActionMode(ActionMode mode) {
+
+      }
+  });
+
+            getLoaderManager().initLoader(ALARM_LOADER, null, this);
 
     }
 
@@ -145,4 +210,49 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
+    void delete_all_student(){
+      int delete_all=  getContentResolver().delete(AlarmEntry.CONTENT_URI,null,null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+            case R.id.delete :
+                show_delete_all_dialog();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    void show_delete_all_dialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();;
+            }
+        });
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                delete_all_student();
+            }
+        });
+        builder.setTitle("Warning");
+        builder.setMessage("Delete ALL Alarms?");
+        AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+
+
+    }
+
 }
